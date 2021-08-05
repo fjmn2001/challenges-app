a5b2717505dae44c002e122f7e1a2ff33f314491
gibmyx <gibmyxjobs@gmail.com>
2021-08-05 08:13:44 -0400

    Feat: app livewire in laravel

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('/invoices/{id}/total', InvoiceTotalController::class);
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
